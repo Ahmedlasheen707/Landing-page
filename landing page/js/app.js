ff6ee715daad8698cdf16043b8fe0812524b5e30
Ahmed Lasheen <78974649+Ahmedlasheen707@users.noreply.github.com>
2021-03-28 15:27:24 +0200

    second commit
    second commit

--- landing page/js/app.js
+++ landing page/js/app.js
@@ -1,13 +1,41 @@
+(() => {
+  // here i will add links to the ul for each section in the page
+  const sections = document.querySelectorAll("section");
+  const navbar = document.querySelector(".links");
+  sections.forEach((section) => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    // here the text will be the id of the section
+    let text = document.createTextNode(section.id);
+    a.appendChild(text);
+    a.classList.add("link");
+    a.href = "";
+    li.appendChild(a);
+    console.log(li);
+    navbar.appendChild(li);
+  });
+})();
+(() => {
+  // here i will scroll to
+  const sections = document.querySelectorAll("section");
+  const links = document.querySelectorAll("li");
+  links.forEach((link, i) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      scroll(0, sections[i].offsetTop - 50);
+    });
+  });
+})();
 // this function scrolls to the top of the page
-function scrollUp() {
-  window.scrollTo(0, 0);
-}
+const scrollUp = () => {
+  window.scroll(0, 0);
+};
 // this function opens the navbar
-function expand() {
+const expand = () => {
   document.querySelector(".links").classList.toggle("open");
-}
+};
 // this function make the up button appear and disappear
-function contorlUp() {
+const contorlUp = () => {
   // if we scrolled more than 500px show it else hide it
   const btn = document.querySelector(".scroll-up");
   if (window.scrollY > 500) {
@@ -17,9 +45,9 @@ function contorlUp() {
     btn.style.opacity = "0";
     btn.style.pointerEvents = "none";
   }
-}
+};
 
-function changeNavColor() {
+const changeNavColor = () => {
   // if we scrolled more than 100px make the navbar gray else leave it the way it was
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 100) {
@@ -27,22 +55,21 @@ function changeNavColor() {
   } else if (window.scrollY < 50) {
     navbar.classList.remove("gray");
   }
-}
+};
 let scrolling = null;
-function hideNav() {
+const hideNav = () => {
   // when we stop scrolling tha last set time out will not be cleared and it will make the nav disappear but if we are at the top of the page it will not disappear
   const navbar = document.querySelector(".navbar");
   navbar.style.top = "0";
   clearTimeout(scrolling);
-  scrolling = setTimeout(function () {
+  scrolling = setTimeout(() => {
     navbar.style.top = "-100%";
   }, 3000);
   if (window.scrollY < 50) {
     clearTimeout(scrolling);
   }
-}
-// if the section's top offest is >= the scroll position highlight its nav item. 
-function activateNavLinks() {
+};
+const activateNavLinks = () => {
   let links = document.querySelectorAll(".link");
   let sectionsOffset = [];
   document.querySelectorAll("section").forEach((section) => {
@@ -60,7 +87,7 @@ function activateNavLinks() {
       });
     }
   });
-}
+};
 window.onscroll = () => {
   contorlUp();
   changeNavColor();
